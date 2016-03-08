636574bbce1676594a311fa9a1f41177fc22c284
Li Hao <Howe0819@gmail.com>
2016-03-08 22:29:25 +0100

    merged post page

--- app/src/main/java/com/example/vin_s/cargo/PostPage.java
+++ app/src/main/java/com/example/vin_s/cargo/PostPage.java
@@ -6,15 +6,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-
+import android.widget.FrameLayout.LayoutParams;
 import com.example.vin_s.cargo.model.Comment;
 import com.example.vin_s.cargo.model.Person;
 import com.example.vin_s.cargo.model.Post;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
+
 
 /**
  * Created by VIN-S on 16/3/7.
@@ -38,8 +41,12 @@ public class PostPage extends AppCompatActivity{
     private Boolean createPostOrNot = false;
     private Boolean selectPostOrNot = false;
     private Person creator;
+
     private String userID;
     private SharedPreferences prefs;
+    private LinearLayout commentLayout;
+    private List<Comment> comments = new ArrayList<Comment>();
+    private String postID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +62,7 @@ public class PostPage extends AppCompatActivity{
         if(selectPostOrNot!=null&&selectPostOrNot){
             postSelected = (Post) getIntent().getSerializableExtra("selectedPost");
             DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+            postID = postSelected.getId();
 
             creator = setCreaterInfo(postSelected.getOwnerID());
 
@@ -112,6 +120,7 @@ public class PostPage extends AppCompatActivity{
 //          get post content and creater info
             postCreated = (Post) getIntent().getSerializableExtra("postCreated");
             creator = setCreaterInfo(postCreated.getOwnerID());
+            postID = postCreated.getId();
 
 //          set content for layout
             title.setText(postCreated.getTitle().toString());
@@ -132,6 +141,28 @@ public class PostPage extends AppCompatActivity{
             seatsLeft.setText("Seats Left: " + postCreated.getSeatsLeft());
             requirements.setText("Special Requirements: " + postCreated.getRequirements());
         }
+
+
+        //comments Layout
+        commentLayout = (LinearLayout) findViewById(R.id.commentLayout);
+
+        LinearLayout commentTitleLayout = new LinearLayout(this);
+        commentTitleLayout.setOrientation(LinearLayout.VERTICAL);
+        commentTitleLayout.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT));
+
+        TextView commentsNumber = new TextView(this);
+        LinearLayout.LayoutParams lpT = new LinearLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+        lpT.setMargins(10,10,0,10);
+        commentsNumber.setLayoutParams(lpT);
+        commentsNumber.setTextSize(14);
+        commentsNumber.setTextColor(0XFF000000);
+
+
+        View view = new View(this);
+        LinearLayout.LayoutParams lpV = new LinearLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,1);
+        view.setLayoutParams(lpV);
+        view.setBackgroundColor(0XFF000000);
+
     }
 
     public Person setCreaterInfo(String personID){
